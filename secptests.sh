4fc38df947b368ded3674e3e070ef07f7173c60d
isle2983 <isle2983@yahoo.com>
2017-03-14 13:32:38 -0600

    TEST

---
+++ secptests.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+git rev-parse HEAD
+echo ""
+cat ~/bin/secptests.sh
+echo ""
+cat /proc/cpuinfo |grep "model name" |uniq -c
+echo ""
+make clean
+echo ""
+./configure --enable-benchmark
+echo ""
+make -j6
+echo ""
+./tests
+echo ""
+./exhaustive_tests
+echo ""
+./bench_internal
+echo ""
+./bench_sign
+echo ""
+./bench_verify
+echo ""
+
+make clean
+echo ""
+./configure --enable-benchmark --enable-endomorphism
+echo ""
+make -j6
+echo ""
+./tests
+echo ""
+./exhaustive_tests
+echo ""
+./bench_internal
+echo ""
+./bench_sign
+echo ""
+./bench_verify
+echo ""
